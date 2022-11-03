b3945cb4edfe02fa1460195c144a5658ae47ce1a
Kauê Samuel <migarati02@gmail.com>
2022-11-03 15:44:43 -0300

    Equação 2 grau bascara
    logica da equção do 2 grau criada ate a parte da bascara criada com sucesso

--- Logicas/Logicas_basicas/src/com/itb/primitive/Equação_2Grau.java
+++ Logicas/Logicas_basicas/src/com/itb/primitive/Equação_2Grau.java
@@ -1,10 +1,27 @@
 package com.itb.primitive;
 
+import java.util.Scanner;
+
 public class Equação_2Grau {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double a;
+		double b;
+		double c;
+		
+		Scanner tcd = new Scanner( System.in);
+		System.out.println("escreva o valor de A");
+		a = tcd.nextDouble();
+		System.out.println("escreva o valor de A");
+		b = tcd.nextDouble();
+		System.out.println("escreva o valor de C");
+		c = tcd.nextDouble();
+		
+		System.out.println(" bascara calculada: ");
+	
+		Double delta = (b * b) - 4 * a * c;
+		
+		System.out.println(delta);
 	}
 
 }
